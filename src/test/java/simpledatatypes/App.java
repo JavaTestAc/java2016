c639b91f499cb69c926022fd271092318069440f
JavaTestAc <aleksandr.g0nchar@yandex.ru>
2016-10-16 22:39:03 +0300

    added App.java to master

---
+++ src/test/java/simpledatatypes/App.java
@@ -0,0 +1,10 @@
+package simpledatatypes;
+
+/**
+ * Created by AG on 16.10.2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
